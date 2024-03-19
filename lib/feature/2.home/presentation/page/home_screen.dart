3f9abbc4e230c39da7ca8d9ca81e759520ac5cc5
“KimDane” <kimdane@twosun.com>
2024-03-19 14:18:00 +0900

    firbase setting

--- lib/feature/2.home/presentation/page/home_screen.dart
+++ lib/feature/2.home/presentation/page/home_screen.dart
@@ -1,3 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 import '../../../../core/constants/index.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
@@ -18,6 +21,40 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   }
 
   Widget home() {
-    return const Text('home screen');
+    return ElevatedButton(
+      onPressed: () {
+        googlelogin();
+      },
+      child: const Text('Login Button'),
+    );
+  }
+
+    Future<void> googlelogin() async {
+     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+      print('>>>> displayName : ${googleUser?.displayName}');
+      print('>>>> email : ${googleUser?.email}');
+      print('>>>> accessToken : ${googleAuth?.accessToken}');
+      print('>>>> idToken : ${googleAuth?.idToken}');
+
+       OAuthCredential _googleCredential = GoogleAuthProvider.credential(
+        idToken: googleAuth?.idToken,
+        accessToken: googleAuth?.accessToken,
+      );
+
+      UserCredential _credential =
+          await FirebaseAuth.instance.signInWithCredential(_googleCredential);
+      if (_credential.user != null) {
+        print(">>>> ${_credential.user}");
+      }
+
+      // Create a new credential
+      // final credential = GoogleAuthProvider.credential(
+      //   accessToken: googleAuth?.accessToken,
+      //   idToken: googleAuth?.idToken,
+      // );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'dart:async';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'app.dart';
 import 'core/constants/index.dart';
 
 FutureOr<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const ProviderScope(child: App()));
 }
